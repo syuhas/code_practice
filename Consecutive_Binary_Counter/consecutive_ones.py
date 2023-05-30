a0ce849f04f2f4cf336a1c3c66528da1e4e77d8e
syuhas <syuhas22@gmail.com>
2023-05-30 15:07:13 -0400

    modified consecutive ones code

--- Consecutive_Binary_Counter/consecutive_ones.py
+++ Consecutive_Binary_Counter/consecutive_ones.py
@@ -5,18 +5,23 @@ def counter(num):
     count = 0
     temp_max = 0
     max_count = 0
+    index = 0
+
     for i in num:
         if i == 1:
             count += 1
+            max_count = max(max_count, count)
         else:
-            temp_max = max(temp_max, count)
-            max_count = temp_max
             count = 0
+
+        print(f"Index {index}: Current count of consecutive ones is {count} and max consecutive ones is {max_count}")
+        index += 1
+
     return max_count
 
 
 
-bin_list = [1,0,1,1,1,1,0,0,1,1]
+bin_list = [1,0,1,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1,0,1,1,0,1,1,1,1,1,1,0,1,1,1,0,1]
 
 
 
--- README.md
+++
@@ -1,4 +0,0 @@
-# code_practice
-
-
-<code>A place for me to put all of my random practice code that I am using to brush up on my skills or just for fun.</code>
